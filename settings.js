ef4cdaf42b587acf3dc2b45f71e6259172e0589b
SYCKO2004 <110967246+SYCKO2004@users.noreply.github.com>
2022-08-10 13:34:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -44,19 +44,19 @@ global.antibad = "false" // To remove bad word senders
 global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "true" // bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94775807293']
+global.ownertag = '+94775807293' //your tag number
+global.botname = 'SYCKO - 𝘉𝘖𝘛' //ur bot name
+global.packname = "SYCKO" // sticker packname
+global.author = "SHANA" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ SYCKO SHANA ]' //
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ YOUTUBE ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://youtube.com/c/SSMODS1' //Change Your Button's URL
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94775807293'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
